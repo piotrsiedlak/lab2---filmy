c9c2e30b29b61bec76498b50393f342ba19d46ae
Piotr Siedlak <piotr.siedlak@microsoft.wsei.edu.pl>
2024-11-14 19:00:42 +0100

    walidacja danych w dane.cs

--- lab2 - filmy/Models/Dane.cs
+++ lab2 - filmy/Models/Dane.cs
@@ -1,11 +1,19 @@
-﻿namespace lab2___filmy.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace lab2___filmy.Models
 {
     public class Dane
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "Prosze podaj Imie")]
         public string Imie { get; set; }
+        [Required(ErrorMessage = "Prosze podaj Email")]
+        [RegularExpression(".+\\@.+\\.[a-z]{2,3}")]
         public string Email { get; set; }
+        [Required(ErrorMessage = "Prosze podaj Temat")]
         public string Temat { get; set; }
+        [Required(ErrorMessage = "Prosze podaj Tresc wiadomosci")]
+        [MinLength(10), MaxLength(50)]
         public string Tresc { get; set; }
     }
 }
